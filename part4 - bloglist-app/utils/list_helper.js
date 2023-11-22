5938de6b17fca8cbca61098d6b5b3212ba35bb4e
rotero08 <roteroerazo2@gmail.com>
2023-11-22 10:52:43 -0500

    exercise 4.5

--- part4 - bloglist-app/utils/list_helper.js
+++ part4 - bloglist-app/utils/list_helper.js
@@ -12,9 +12,25 @@ const totalLikes = (blogs) => {
         : blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const reducer = (acc, blog) => {
+        if (!acc || (blog.likes && blog.likes > acc.likes)) {
+            return blog;
+          }
+        return acc;
+    }
 
+    mostLiked = null
+
+    blogs.length === 0
+        ? 0
+        : mostLiked = blogs.reduce(reducer, null)
+    
+        return mostLiked ? { title: mostLiked.title, author: mostLiked.author, likes: mostLiked.likes } : null;
+}
 
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
 }
