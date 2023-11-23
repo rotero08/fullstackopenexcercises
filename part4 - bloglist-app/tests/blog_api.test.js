4242ae600a11fd19b90df75a6bbe8a277dd7c8e6
rotero08 <roteroerazo2@gmail.com>
2023-11-23 17:34:27 -0500

    exercise 4.9

--- part4 - bloglist-app/tests/blog_api.test.js
+++ part4 - bloglist-app/tests/blog_api.test.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose')
+const helper = require("./test_helper")
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    const blogObjects = helper.initialBlogs
+      .map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+})
 
 const api = supertest(app)
 
@@ -9,6 +19,17 @@ test('blogs are returned as json', async () => {
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('blogs unique identifier is named id', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const blogObjets = response.body
+    const promiseArray = blogObjets.map(blog => expect(blog.id).toBeDefined())
+    await Promise.all(promiseArray)
 })
 
 afterAll(async () => {
