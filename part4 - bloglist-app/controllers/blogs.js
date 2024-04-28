85c8c695267d45e079dba37eb83b5449f0051bbe
rotero08 <roteroerazo2@gmail.com>
2024-04-28 18:04:18 -0500

    exercise 4.19

--- part4 - bloglist-app/controllers/blogs.js
+++ part4 - bloglist-app/controllers/blogs.js
@@ -1,7 +1,16 @@
 const blogsRouter = require("express").Router()
 const { response } = require("../app")
+const jwt = require('jsonwebtoken')
 const User = require("../models/user")
 const Blog = require("../models/blog")
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -11,7 +20,11 @@ blogsRouter.get('/', async (request, response) => {
   
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const user = await User.findOne({});
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   if (!user) return response.status(404).json({ error: 'No users found' });
 
@@ -19,6 +32,7 @@ blogsRouter.post('/', async (request, response) => {
     ...body,
     user: user.id
   });
+  
   const savedBlog = await blog.save()
 
   user.blogs = user.blogs.concat(savedBlog._id)
