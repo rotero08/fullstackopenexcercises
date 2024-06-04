c06908fb2134fc8d38e959ba69120837251ae257
rotero08 <roteroerazo2@gmail.com>
2024-06-03 20:23:29 -0500

    exercise 6.14

--- part6/redux-anecdotes-main/src/App.jsx
+++ part6/redux-anecdotes-main/src/App.jsx
@@ -1,10 +1,19 @@
+import { useEffect } from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import noteService from './services/noteService'
+import { setNotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
-
+  const dispatch = useDispatch()
+  useEffect(() => {
+    noteService
+      .getAll().then(notes => dispatch(setNotes(notes)))
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <div>
--- part6/redux-anecdotes-main/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes-main/src/reducers/anecdoteReducer.js
@@ -1,29 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
 
 const noteSlice = createSlice({
   name: 'notes',
-  initialState,
+  initialState: [],
   reducers: {
     createNote(state, action) {
       const content = action.payload
@@ -41,9 +22,15 @@ const noteSlice = createSlice({
         votes: noteToChange.votes + 1
       }
       return state.map(note => note.id !== id ? note : changedNote)
+    },
+    appendNote(state, action) {
+      state.push(action.payload)
+    },
+    setNotes(state, action) {
+      return action.payload
     }
   }
 })
 
-export const { createNote, incrementVote } = noteSlice.actions
+export const { createNote, incrementVote, appendNote, setNotes } = noteSlice.actions
 export default noteSlice.reducer
