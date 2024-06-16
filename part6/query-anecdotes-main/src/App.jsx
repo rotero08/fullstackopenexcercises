0072065db9ece080f01764222766dbfb5cf4c5e0
rotero08 <roteroerazo2@gmail.com>
2024-06-16 02:48:30 -0500

    exercise 6.22

--- part6/query-anecdotes-main/src/App.jsx
+++ part6/query-anecdotes-main/src/App.jsx
@@ -1,12 +1,20 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { useQuery } from '@tanstack/react-query'
-import { getNotes } from './requests'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getNotes, updateNote } from './requests'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const updateNoteMutation = useMutation({
+    mutationFn: updateNote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    },
+  })
 
   const handleVote = (anecdote) => {
-    console.log('vote')
+    updateNoteMutation.mutate({...anecdote, votes: anecdote.votes+1 })
   }
 
   const result = useQuery({
--- part6/query-anecdotes-main/src/requests.js
+++ part6/query-anecdotes-main/src/requests.js
@@ -8,3 +8,6 @@ export const getNotes = () =>
 
 export const createNote = newNote =>
   axios.post(baseUrl, newNote).then(res => res.data)
+
+export const updateNote = updatedNote =>
+    axios.put(`${baseUrl}/${updatedNote.id}`, updatedNote).then(res => res.data)
